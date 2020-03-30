a2eb3217acace505eabc29ce62e72c0bcb9be2c6
jay <jay@jayjay.local>
2020-03-30 21:58:54 +0200

    add AddTeam component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './layout/header/header.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./layout/header/header.component";
+import { MatchComponent } from "./components/match/match.component";
+import { TeamComponent } from "./components/team/team.component";
+import { PlayersComponent } from "./components/players/players.component";
+import { AddMatchComponent } from "./components/add-match/add-match.component";
+import { AddTeamComponent } from './components/add-team/add-team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MatchComponent,
+    TeamComponent,
+    PlayersComponent,
+    AddMatchComponent,
+    AddTeamComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
